8e127e6f47bad47f0b2c6794980dcb3f5f1ea4ba
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-18 14:18:02 -0800

    practice with function types

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 const add = (n1: number, n2: number) => {
   return n1 + n2;
-}
+};
 
 const printResult = (num: number): void => {
-  console.log('Result: ' + num); 
-}
+  console.log("Result: " + num);
+};
 
-printResult(add(5, 12))
+printResult(add(5, 12));
+
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+// combinedValues = printResult;
+
+console.log(combinedValues(8, 8));
