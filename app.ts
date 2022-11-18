72da8d6b531313d8de155c17fd707db810e2aff3
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-18 14:22:50 -0800

    practice with function callbacks types

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ const printResult = (num: number): void => {
   console.log("Result: " + num);
 };
 
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+  const result = n1 + n2;
+  cb(result);
+};
+
 printResult(add(5, 12));
 
 let combinedValues: (a: number, b: number) => number;
@@ -14,3 +19,7 @@ combinedValues = add;
 // combinedValues = printResult;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
