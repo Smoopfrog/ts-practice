cacd50de7bf1c0188d08ceefdb9e1bd7a9e4ed7f
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-18 12:59:04 -0800

    practice with tuples

--- app.ts
+++ app.ts
@@ -1,18 +1,25 @@
-// const person: {
-  // name: string;
-  // age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  // const person = {
   name: "Jeff",
   age: 28,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
+// person.role.push("admin");
+// person.role[1] = 10;
+person.role = [0, "admin"];
+
 let favouriteActivities: string[];
-favouriteActivities = ['Games']
+favouriteActivities = ["Games"];
 
 console.log(person);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase())
+  console.log(hobby.toUpperCase());
 }
