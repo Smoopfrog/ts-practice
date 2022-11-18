8a3f0784c0f6545fe9ae29f16a50c47b7502c481
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-18 12:27:04 -0800

    practice add function

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here!');
+const add = (n1: number, n2: number, showResult: boolean) => {
+  console.log(typeof number1)
+  if (showResult) {
+    console.log(n1 + n2)
+  } else {
+    return n1 + n2;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+
+add(number1, number2, printResult);
