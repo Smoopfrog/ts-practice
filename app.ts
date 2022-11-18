f4bc7c714a2cd9d34cfeb0ced454df15cc6c7d43
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-11-18 13:50:50 -0800

    practice with union types

--- app.ts
+++ app.ts
@@ -1,42 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   // const person = {
-//   name: "Jeff",
-//   age: 28,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// };
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-//Enums increment automatically, can be set to whatever, and will increment if after a number is set
-enum Role {ADMIN = 'Admin', READ_ONLY = 100, AUTHOR = 200};
-
-const person = {
-  name: "Jeff",
-  age: 28,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN
+const combine = (input1: number | string, input2: number | string) => {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toLocaleString();
+  }
+  return result;
 };
 
-// person.role.push("admin");
-// person.role[1] = 10;
-// person.role = [0, "admin"];
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-let favouriteActivities: string[];
-favouriteActivities = ["Games"];
-
-console.log(person);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log('Is author')
-}
+const combinedNames = combine("Max", "Anna");
+console.log(combinedNames)
